82ddb73ce698c901cc1542de691207eb720f0ea6
Ted M. Young <tedyoung@gmail.com>
2022-09-28 11:30:49 -0700

    LAB #2: Added characterization test for displayFaceUpCard

---
+++ src/test/java/com/jitterted/ebp/blackjack/HandDisplayTest.java
@@ -0,0 +1,21 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class HandDisplayTest {
+
+    @Test
+    public void displayFaceUpCard() throws Exception {
+        Hand hand = new Hand(List.of(new Card(Suit.HEARTS, Rank.ACE)));
+
+        assertThat(hand.displayFaceUpCard())
+                .isEqualTo("[31m┌─────────┐[1B[11D│A        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        A│[1B[11D└─────────┘");
+    }
+
+
+}
+
