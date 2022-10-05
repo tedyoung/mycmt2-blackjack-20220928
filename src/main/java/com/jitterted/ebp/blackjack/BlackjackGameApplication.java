77649ee6ccd7132fccd06a80e30c714827451760
Ted M. Young <tedyoung@gmail.com>
2022-10-05 10:45:24 -0700

    TDD: Make Spring happy by providing a way (in the "configurator") for it to get a reference to an instance of Game

--- src/main/java/com/jitterted/ebp/blackjack/BlackjackGameApplication.java
+++ src/main/java/com/jitterted/ebp/blackjack/BlackjackGameApplication.java
@@ -1,13 +1,21 @@
 package com.jitterted.ebp.blackjack;
 
+import com.jitterted.ebp.blackjack.domain.Deck;
+import com.jitterted.ebp.blackjack.domain.Game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlackjackGameApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlackjackGameApplication.class, args);
+    }
+
+    @Bean
+    public Game createGame() {
+        return new Game(new Deck());
     }
 
 }
--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
@@ -1,14 +1,16 @@
 package com.jitterted.ebp.blackjack.adapter.in.web;
 
 import com.jitterted.ebp.blackjack.domain.Game;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BlackjackController {
 
-    private final Game game;
+    private final Game game; // ENTITY-like class
 
+    @Autowired
     public BlackjackController(Game game) {
         this.game = game;
     }
