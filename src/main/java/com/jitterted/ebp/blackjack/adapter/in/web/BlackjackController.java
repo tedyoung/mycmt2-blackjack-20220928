8ed792ebfa764634b7719163a1462938d27d915c
Ted M. Young <tedyoung@gmail.com>
2022-10-05 10:50:31 -0700

    TDD: GET to /game endpoint is status 200 OK

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
@@ -3,6 +3,7 @@ package com.jitterted.ebp.blackjack.adapter.in.web;
 import com.jitterted.ebp.blackjack.domain.Game;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -19,6 +20,11 @@ public class BlackjackController {
     public String startGame() {
         game.initialDeal();
         return "redirect:/game";
+    }
+
+    @GetMapping("/game")
+    public String gameView() {
+        return "blackjack";
     }
 
 }
--- src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/WebIntegrationTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/WebIntegrationTest.java
@@ -27,5 +27,10 @@ public class WebIntegrationTest {
                .andExpect(status().is3xxRedirection());
     }
 
+    @Test
+    public void getRequestToGameEndpointIsStatus200Ok() throws Exception {
+        mockMvc.perform(get("/game"))
+               .andExpect(status().isOk());
+    }
 
 }
